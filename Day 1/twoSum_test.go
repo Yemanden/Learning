43e5f34a69d85aaf3acfcc24c4eed4a3b92e446e
Yemanden <V1997V@rambler.ru>
2019-12-14 17:51:39 +0300

    Добавление решения первого дня и тестов к нему

---
+++ Day 1/twoSum_test.go
@@ -0,0 +1,17 @@
+package Day_1
+
+import (
+	"reflect"
+	"testing"
+)
+
+//Тест для функции "twoSum"
+func TestTwoSum(t *testing.T) {
+	numbers := []int{1, 2, 3, 4, 5}
+
+	got := twoSum(numbers, 3)
+	want := []int{0, 1}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
