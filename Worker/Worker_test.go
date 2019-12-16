fc79d1b18be9498223fe45dc34267461fa008fbf
Yemanden <V1997V@rambler.ru>
2019-12-16 13:59:03 +0200

    Исправление аргумента функции в тесте Исправление опечатки в комментарии к тесту

--- Worker/Worker_test.go
+++ Worker/Worker_test.go
@@ -2,10 +2,10 @@ package Worker
 
 import "testing"
 
-// BenchmarkWork проверяет среднее значение скорости выполнение функции Work
+// BenchmarkWork проверяет среднее значение скорости выполнения функции Work
 // при значениях аргументов от 1 до 10
 func BenchmarkWork(b *testing.B) {
 	for i := 1; i <= 10; i++ {
-		Work(1)
+		Work(uint(i))
 	}
 }
