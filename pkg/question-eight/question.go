2c23260c4a25ecb3d17125d124f16100eaf2918c
Yemanden <V1997V@rambler.ru>
2020-01-08 14:34:34 +0300

    Добавлены интерфейсы к задаче с сайта LeetCode.

--- pkg/question-eight/question.go
+++ pkg/question-eight/question.go
@@ -1,25 +1,52 @@
 package question_eight
 
-type treeNode struct {
-	val   int
-	left  *treeNode
-	right *treeNode
+// TreeSummarizer ...
+type TreeSummarizer interface {
+	TreeSummarize(int, int) int
 }
 
-func rangeSumBST(root *treeNode, L int, R int) int {
+// TreeInitializationer ...
+type TreeInitializationer interface {
+	TreeInitialization(int, treeNoder, treeNoder)
+}
+
+type treeNoder interface {
+	TreeSummarizer
+	TreeInitializationer
+}
+
+type treeNode struct {
+	val   int
+	left  treeNoder
+	right treeNoder
+}
+
+// TreeSummarize ...
+func (t *treeNode) TreeSummarize(L int, R int) int {
 	var sum int
-	if root == nil {
+	if t == nil {
 		return sum
 	}
 
-	if root.val >= L && root.val <= R {
-		sum += root.val
+	if t.val >= L && t.val <= R {
+		sum += t.val
 	}
-	if root.val > L {
-		sum += rangeSumBST(root.left, L, R)
+	if t.val > L {
+		sum += t.left.TreeSummarize(L, R)
 	}
-	if root.val <= R {
-		sum += rangeSumBST(root.right, L, R)
+	if t.val <= R {
+		sum += t.right.TreeSummarize(L, R)
 	}
 	return sum
+}
+
+// TreeInitialization ...
+func (t *treeNode) TreeInitialization(val int, left, right treeNoder) {
+	t.val = val
+	t.left = left
+}
+
+/// NewTreeNode ...
+func NewTreeNode() treeNoder {
+	return &treeNode{}
 }
